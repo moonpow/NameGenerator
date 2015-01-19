30e70ba4ebe3f17011f568ac90bb6b80677af5b2
Kalen <0moonpow0@gmail.com>
2015-01-19 15:48:09 -0800

    Initial commit

---
+++ src/namePicker/NamePicker.java
@@ -0,0 +1,34 @@
+package namePicker;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NamePicker {
+	public static void main(String args[]){
+		Random random = new Random();
+		Scanner scan = new Scanner(System.in);
+		String nameS[] = {"Zan", "Can", "Lan", "Dan", "Stel", "Aren", "Air", "Stan", "San", "Luk", "Cal", "Med", "Joe", "Loh", "Dra", 
+				"Lohk", "Ipt", "Ar", "Tal", "Sharp"};
+		
+		String nameM[] = {"av", "drec", "ouc", "mern", "lis", "don", "cam", "com", "dar", "port", "toc", "on", "ie", "polt", "gon"};
+	
+		String nameE[] = {"er", "mair", "ler", "dra", "co", "las", "ner", "lair", "mar", "bon", "mes", "ar", "ire", "ore", "born", "ite",
+				"tigh", "polt", "nam", "turn"};
+		
+		System.out.println(nameS.length + ", " + nameM.length + ", " + nameE.length);
+		System.out.println("How many names would you like to create?");
+		int amount = scan.nextInt();
+		
+		for (int i = 0; i < amount; ++i){
+			if (Math.random() * 100 > 70){
+				System.out.print(nameS[random.nextInt(nameS.length)]);
+				System.out.println(nameE[random.nextInt(nameE.length)]);
+			}else{
+				System.out.print(nameS[random.nextInt(nameS.length)]);
+				System.out.print(nameM[random.nextInt(nameM.length)]);
+				System.out.println(nameE[random.nextInt(nameE.length)]);
+			}
+		}
+	}
+
+}
