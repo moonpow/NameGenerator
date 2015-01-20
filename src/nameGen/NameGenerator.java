50a03f444782a5e35484671e75f294c809379341
Quest Osucha <combineturret@gmail.com>
2015-01-19 17:06:35 -0800

    +Some Endings, Some Beginnings *Closed scanner

--- src/nameGen/NameGenerator.java
+++ src/nameGen/NameGenerator.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 public class NameGenerator {
 	public static void main(String args[]){
 		Random random = new Random();
-		Scanner scan = new Scanner(System.in);
 		String nameS[] = {"Zan", "Can", "Lan", "Dan", "Stel", "Aren", "Air", "Stan", "San", "Luk", "Cal", "Med", "Joe", "Loh", "Dra", 
-				"Lohk", "Ipt", "Ar", "Tal", "Sharp"};
+				"Lohk", "Ipt", "Ar", "Tal", "Sharp", "Mer"};
 		
 		String nameM[] = {"av", "drec", "ouc", "mern", "lis", "don", "cam", "com", "dar", "port", "toc", "on", "ie", "polt", "gon"};
 	
 		String nameE[] = {"er", "mair", "ler", "dra", "co", "las", "ner", "lair", "mar", "bon", "mes", "ar", "ire", "ore", "born", "ite",
-				"tigh", "polt", "nam", "turn", "lock"};
+				"tigh", "polt", "nam", "turn", "lock", "lynn"};
 		
 		System.out.println(nameS.length + ", " + nameM.length + ", " + nameE.length);
 		System.out.println("How many names would you like to create?");
+		Scanner scan = new Scanner(System.in);
 		int amount = scan.nextInt();
+		scan.close();
 		
 		for (int i = 0; i < amount; ++i){
 			if (Math.random() * 100 > 70){
